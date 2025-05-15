5e4f0551df5b2438896b6265b45e40e0cb6e9d46
ndriqa <cimilimi1998@gmail.com>
2025-05-16 00:18:50 +0200

    modify preferred visualizer selection ui

--- app/src/main/java/com/ndriqa/musicky/features/settings/components/SettingPreferredVisualizer.kt
+++ app/src/main/java/com/ndriqa/musicky/features/settings/components/SettingPreferredVisualizer.kt
@@ -15,6 +15,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ExpandLess
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
@@ -31,8 +33,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.ndriqa.musicky.R
 import com.ndriqa.musicky.core.data.AudioFeatures
 import com.ndriqa.musicky.core.data.FftFeatures
@@ -58,7 +62,31 @@ internal fun SettingPreferredVisualizer(
             else Modifier
         ),
         content = {
-            SettingsItemTitle(stringResource(R.string.preferred_visualizer))
+            AnimatedContent(isExpanded) { expanded ->
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    val title =
+                        if (expanded) preferredVisualizer.title
+                        else stringResource(R.string.preferred_visualizer)
+
+                    Text(
+                        text = title,
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.weight(1f),
+                        textAlign = if (expanded) TextAlign.Left else TextAlign.Center
+                    )
+
+                    if (expanded) {
+                        Icon(
+                            imageVector = Icons.Rounded.ExpandLess,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
             Spacer(modifier = Modifier.height(PaddingMini))
             Box {
                 fun onVisualizerTileClick(type: VisualizerType) {
